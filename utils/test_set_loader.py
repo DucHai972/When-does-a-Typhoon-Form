36b6b15a44a32adf8d5a36543e14b51c9f631f23
Nguyễn Đức Hải <110242130+DucHai972@users.noreply.github.com>
2024-07-09 13:24:33 +0700

    Update test_set_loader.py

--- utils/test_set_loader.py
+++ utils/test_set_loader.py
@@ -1,21 +1,33 @@
 import gdown
 import os
+import patoolib
 
 def load_test_set():
-    model_drive_id = '1UAsJ6LESJHLaw8cATerGZ7oAFAUp99Th'
-    drive_url = f'https://drive.google.com/uc?id={model_drive_id}'
+    data_drive_id = '14-XfVCVSpBdqNfirHdD31mjRFLVvkeRQ'
+    drive_url = f'https://drive.google.com/uc?id={data_drive_id}'
     
-    output_path = './data/test_set.npy'
-    
-    # Check if the file already exists
-    if not os.path.exists(output_path):
-        print(f"{output_path} not found. Downloading from {drive_url}...")
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        gdown.download(drive_url, output_path, quiet=False)
-        print(f"File downloaded and saved as {output_path}")
-    else:
-        print(f"{output_path} already exists. Skipping download.")
+    rar_path = './data/testdata_2019255.rar'
+    extract_path = './data/test_set'
 
-if __name__ == '__main__':
-    load_test_set()
+    # Check if the extracted data already exists
+    if not os.path.exists(extract_path):
+        print(f"{extract_path} not found. Downloading from {drive_url}...")
+        os.makedirs(os.path.dirname(rar_path), exist_ok=True)
+        
+        # Download the .rar file
+        gdown.download(drive_url, rar_path, quiet=False)
+        print(f"File downloaded and saved as {rar_path}")
+        
+        # Extract the .rar file
+        print(f"Extracting {rar_path} to {extract_path}...")
+        patoolib.extract_archive(rar_path, outdir=extract_path)
+        print(f"Extraction completed and saved at {extract_path}")
+        
+        # Remove .rar file
+        os.remove(rar_path)
+        print(f"Removed the .rar file: {rar_path}")
+    else:
+        print(f"{extract_path} already exists. Skipping download and extraction.")
+
+
 
