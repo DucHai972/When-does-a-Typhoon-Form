c0e5121d4091565dac5a8a8c05ba4b5e5deeee0d
Nguyễn Đức Hải <110242130+DucHai972@users.noreply.github.com>
2024-07-09 13:22:22 +0700

    Update label_loader.py

--- utils/label_loader.py
+++ utils/label_loader.py
@@ -2,12 +2,13 @@ import numpy as np
 
 def create_labels(model_name='resnet_t5.h5'):
     model_configs = {
-        'resnet_t2.h5': (236, 2105),
-        'resnet_t4.h5': (390, 1951),
-        'resnet_t5.h5': (467, 1874),
-        'resnet_t8.h5': (697, 1644),
-        'resnet_t12.h5': (988, 1353),
-        'resnet_t16.h5': (1261, 1080)
+        'resnet_t2.h5': (3, 34),
+        'resnet_t4.h5': (5, 32),
+        'resnet_t5.h5': (6, 31),
+        'resnet_t8.h5': (9, 28),
+        'resnet_t12.h5': (13, 24),
+        'resnet_t16.h5': (17, 20),
+        'cnn_t5.h5': (6, 31)
     }
 
     if model_name not in model_configs:
@@ -16,16 +17,5 @@ def create_labels(model_name='resnet_t5.h5'):
     num_ones, num_zeros = model_configs[model_name]
 
     # Create the labels array
-    labels = np.array([0] * num_zeros + [1] * num_ones)
-    print(num_zeros, num_ones)
+    labels = np.array([1] * num_ones + [0] * num_zeros)
     return labels
-
-def main():
-    #Testing
-    model_name = 'resnet_t5.h5'
-    labels = create_labels(model_name)
-    print(f"Labels for {model_name}:")
-    print(labels)
-
-if __name__ == '__main__':
-    main()
